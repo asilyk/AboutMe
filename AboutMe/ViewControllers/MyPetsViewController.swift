cf0d0ab5bd9e0b2e13cd49adbd5c56c5e8ed6f62
fabet <asilyk112@gmail.com>
2022-01-10 08:39:09 +0300

    Code refactoring

--- AboutMe/ViewControllers/MyPetsViewController.swift
+++ AboutMe/ViewControllers/MyPetsViewController.swift
@@ -12,8 +12,8 @@ class MyPetsViewController: UIViewController {
     @IBOutlet private var firstPetImageView: UIImageView!
     @IBOutlet private var firstPetLabel: UILabel!
 
-    @IBOutlet private var secondPetImageView: UIImageView!
-    @IBOutlet private var secondPetLabel: UILabel!
+    @IBOutlet private var lastPetImageView: UIImageView!
+    @IBOutlet private var lastPetLabel: UILabel!
 
     //MARK: - Public Properties
     var pets: [Pet] = []
@@ -22,12 +22,16 @@ class MyPetsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        firstPetImageView.image = pets.first?.image
-        firstPetImageView.layer.cornerRadius = firstPetImageView.frame.height / 2
-        firstPetLabel.text = "My \(pets.first?.animal.rawValue ?? "") is named \(pets.first?.name ?? "")."
+        setupOutlets(pet: pets.first, imageView: firstPetImageView, label: firstPetLabel)
+        setupOutlets(pet: pets.last, imageView: lastPetImageView, label: lastPetLabel)
+    }
 
-        secondPetImageView.image = pets.last?.image
-        secondPetImageView.layer.cornerRadius = secondPetImageView.frame.height / 2
-        secondPetLabel.text = "My \(pets.last?.animal.rawValue ?? "") is named \(pets.last?.name ?? "")."
+    //MARK: - Private Methods
+    private func setupOutlets(pet: Pet?, imageView: UIImageView, label: UILabel) {
+        guard let pet = pet else { return }
+
+        imageView.image = pet.image
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+        label.text = "My \(pet.animal.rawValue) is named \(pet.name)."
     }
 }
